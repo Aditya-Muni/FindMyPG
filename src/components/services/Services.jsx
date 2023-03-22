40f5fa4af474ac2ae3e62de9456376f833d3a9e2
Aditya <adityamuni2001@gmail.com>
2023-03-22 07:41:11 +0530

    updated

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,20 +1,20 @@
-import React from "react"
-import img from "../images/services.jpg"
-import Back from "../common/Back"
-import "../home/featured/Featured.css"
-import FeaturedCard from "../home/featured/FeaturedCard"
+import React from "react";
+import img from "../images/banner.png";
+import Back from "../common/Back";
+import "../home/featured/Featured.css";
+import FeaturedCard from "../home/featured/FeaturedCard";
 
 const Services = () => {
   return (
     <>
-      <section className='services mb'>
-        <Back name='Services' title='Services -All Services' cover={img} />
-        <div className='featured container'>
+      <section className="services mb">
+        <Back name="Services" title="Services -All Services" cover={img} />
+        <div className="featured container">
           <FeaturedCard />
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
